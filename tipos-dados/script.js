5808ff9bbb5644754256bc84fc5909b1de5617d3
Rebeca Baptista Fonseca Viana <re.bfviana@gmail.com>
2021-11-15 20:36:30 -0300

    adiciona pasta tipos-dados

---
+++ tipos-dados/script.js
@@ -0,0 +1,26 @@
+var myName = "Rebeca"; // string
+
+let language = "JavaScript";
+
+const pattern = "ECMAScript";
+
+{ // escopo 
+    var age = 23; // number
+    let height = 1.64 // number
+}
+
+var isStudent = false; //boolean
+
+var person = { //objeto
+    "name": "Rebeca",
+    "age": 23
+}
+
+var students = [ "Rebeca", "Maria", "Joao" ];
+
+function sum(a,b){ //função
+    return a + b;
+}
+
+//operador typeof - saber o tipo de dado
+console.log (typeof students)
