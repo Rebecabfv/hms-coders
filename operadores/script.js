c758d146c9c308ce57345351b4cde0583536d270
Rebeca Baptista Fonseca Viana <re.bfviana@gmail.com>
2021-11-16 07:43:34 -0300

    Adiciona pasta /operadores

---
+++ operadores/script.js
@@ -0,0 +1,25 @@
+// operadores de atribuição
+// = atribuição
+
+// atribuição de operadores
+
+var x = 1;
+var y = 3;
+console.log(x+=y); // -> x = x+y
+console.log(x-=y);
+console.log(x*=y);
+console.log(x/=y);
+console.log(x%=y);
+
+// operadores de comparação 
+
+// == 
+// === comparar tipo e valor
+// != diferente de
+// < ou <=
+// > ou >==
+
+// operadores lógicos
+// e &&
+// ou ||
+// negação !
