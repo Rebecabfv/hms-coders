9afd9a8a418ea2ab1a868ab807c3bfaefc9ceb9b
Rebeca Baptista Fonseca Viana <re.bfviana@gmail.com>
2021-11-17 15:37:41 -0300

    Adiciona arquivos .gitignore, packege-lock.json, package.json e modifica arquivos app.js e database.js

---
+++ js-avancado/projeto-final.js/app.js
@@ -0,0 +1,34 @@
+// No terminal para iniciar um projeto node: npm init -y --> cria o arquivo package.json (info do projeto e alteração do scripts para start e o arquivo que vamos iniciar o projeto - app.js)
+// Biblioteca readline-sync: npm i readline-sync --> permite que recebermos entrada do usuário
+
+// Importar os dados 
+const livros = require('./database.js');
+console.log(livros);
+
+// Pegar o input do usuário
+const readline = require('readline-sync');
+
+const entradaInicial = readline.question('Deseja buscar um livro? S/N');
+
+// Se for sim, mostra as categorias disponíveis, pergunta qual categoria ela escolhe
+if (entradaInicial.toLocaleUpperCase() === 'S'){
+    console.log('Essas são as categorias disponíveis:');
+    console.log('Produtividade', '/ História brasileira', '/ Américas', '/ Tecnologia', '/ Estilo de vida');
+
+    const entradaCategoria = readline.question('Qual categoria você escolhe?');
+
+    //Retornando de acordo com a categoria escolhida:
+    const retorno = livros.filter(livro => livro.categoria === entradaCategoria);
+
+    console.table(retorno);
+
+// Se for não, mostra todos os livros em ordem crescente pela quantidade de páginas
+} else {
+    //Ordenar os livros de acordo com o número de páginas
+    const livrosOrdenados = livros.sort((a,b) => a.paginas - b.paginas);
+    console.log('Esses são todos os livros disponíveis:');
+    console.table(livrosOrdenados);
+}
+
+
+
---
+++ js-avancado/projeto-final.js/database.js
@@ -0,0 +1,50 @@
+const livros = [
+    {
+        id: 1,
+        nome: "Digital Minimalism",
+        autor: "Cal Newport",
+        categoria: "Produtividade ",
+        paginas: 254,
+        recomenda: false,
+        leu: false  
+    },
+    {
+        id: 2,
+        nome: "O Genocídio do negro brasileiro",
+        autor: "Abdias do Nascimento",
+        categoria: "História brasileira",
+        paginas: 255,
+        recomenda: false,
+        leu: false 
+    },
+    {
+        id: 3,
+        nome: "As veias abertas da américa latina",
+        autor: "Eduardo Galeano",
+        categoria: "Américas",
+        paginas: 400,
+        recomenda: false,
+        leu: false 
+    },
+    {
+        id: 4,
+        nome: "Algoritmos para viver",
+        autor: "Brian Christian",
+        categoria: "Tecnologia",
+        paginas: 412,
+        recomenda: true,
+        leu: true 
+    },
+    {
+        id: 5,
+        nome: "Thinking, fast and slow",
+        autor: "Daniel Kahneman",
+        categoria: "Estilo de vida",
+        paginas: 418,
+        recomenda: true,
+        leu: false 
+    }
+]
+
+//Exportar os dados
+module.exports = livros;
