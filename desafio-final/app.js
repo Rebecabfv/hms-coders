7544f7e6c9d032a498dcbb046d193c091492c70d
Rebeca Baptista Fonseca Viana <re.bfviana@gmail.com>
2021-11-24 22:55:06 -0300

    Importa dados do arquivo database.js

---
+++ desafio-final/app.js
@@ -0,0 +1,13 @@
+// Importar os dados 
+const cursos = require('./database.js');
+
+/* Funções que precisa ser criada:
+● criarCurso: recebe todos os dados por parâmetro e adiciona na lista de cursos
+como objeto.
+● exibirCurso: recebe o id de um curso por parâmetro e imprime todos os dados
+do curso selecionado.
+
+● atualizarCurso: recebe todos os dados por parâmetro e atualiza na lista de
+cursos.
+● deletarCurso: recebe o id por parâmetro e remove o curso selecionado da lista.
+● listaCursos: imprime todos os cursos.*/
