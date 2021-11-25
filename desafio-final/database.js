399f8345cd06f9960f5eda3278e26d851e419c35
Rebeca Baptista Fonseca Viana <re.bfviana@gmail.com>
2021-11-24 22:51:31 -0300

    Cria um exemplo de banco de dados e exporta os dados

---
+++ desafio-final/database.js
@@ -0,0 +1,22 @@
+const cursos = [
+    {
+        id: 1,
+        titulo: Financeiro,
+        descricao: Financeiro,
+        imagem: "recebe uma url",
+        nome_professor: "Marcos",
+        lista_aulas: "recebe links das aulas"
+    },
+
+    {
+        id: 1,
+        titulo: Financeiro,
+        descricao: Financeiro,
+        imagem: "recebe uma url",
+        nome_professor: "Marcos",
+        lista_aulas: "recebe links das aulas"
+    }
+]
+
+//Exportar os dados
+module.exports = livros;
