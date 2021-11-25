94b7ca26861702e2598922f54458657564b2acc0
Rebeca Baptista Fonseca Viana <re.bfviana@gmail.com>
2021-11-24 23:41:18 -0300

    Cria funcao listaCursos

--- desafio-final/app.js
+++ desafio-final/app.js
@@ -4,6 +4,7 @@ const cursos = require('./database.js');
 // Pegar o input do usuário
 const readline = require('readline-sync');
 
+
 /* Funções que precisa ser criada:
 ● criarCurso: recebe todos os dados por parâmetro e adiciona na lista de cursos
 como objeto.
@@ -15,9 +16,22 @@ cursos.
 ● deletarCurso: recebe o id por parâmetro e remove o curso selecionado da lista.
 ● listaCursos: imprime todos os cursos.*/
 
+function exibirCurso(){
+    console.log('Essas são os cursos disponíveis:');
+    // Pegando cada titulo dos objetos
+    cursos.map(curso =>console.log(curso.titulo));
+
+    const entradaCurso = readline.question('Qual curso voce escolhe?');
+
+    //Retornando de acordo com o curso escolhida:
+    const retorno = cursos.filter(curso => curso.titulo === entradaCurso);
+    console.table(retorno);
+}
+
 function listaCursos (){
     console.log('Esses são todos os cursos disponíveis:');
     console.table(cursos);
 }
 
+exibirCurso ();
 listaCursos();
